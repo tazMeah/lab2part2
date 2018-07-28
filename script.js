c53c2cf07987b65037e363ac345e4b803cff8fa2
tazMeah <montazmeahii@yahoo.com>
2018-07-28 01:30:19 -0400

    ADD button now adds inputs to DOM

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 "use strict";
+
 // constructor for AddressBook 
 function AddressBook () {
+
     // array to hold the contacts
     this.list = [];
 
-
-
-
     // function to add to the array
     this.add = function () {
-        // this.list.push(John);
-        // console.log(this.list)
 
         // get contents of inputs
         let name = document.querySelector("#name").value;
@@ -20,15 +17,22 @@ function AddressBook () {
 
         // send the inputs through the Contact constructor
         this.list.push(new Contact(name, email, phone, select));
-        console.log(this.list);
 
-         
+        refreshList();
+
+
+
+        
+        
     }
 }
 
 
 // initializing the AddressBook object
 const addressBook = new AddressBook();
+
+
+
 
 // constructor for Contacts
 function Contact (name, email, phone, relation) {
@@ -38,18 +42,45 @@ function Contact (name, email, phone, relation) {
     this.relation =relation;
 }
 
-// initializing one Contact
+// // initializing one Contact
+// let John = new Contact("John", "email", "7779311", "friend");
+
+// //and adding that one contact to the list
+// addressBook.list.push(John);
 
 
-// test area
 
 
-let John = new Contact("John", "email", "7779311", "friend");
-let Mike = new Contact("John", "email", "7779311", "friend");
-let Brandon = new Contact("John", "email", "7779311", "friend");
-let Samantha = new Contact("John", "email", "7779311", "friend");
-let Dominique = new Contact("John", "email", "7779311", "friend");
-addressBook.list.push(John);
+
+/* * * * * * * * *       test area      * * * * * * * */
 
 
+
+//select the contacts div
 let domContacts = document.querySelector("#contacts");
+
+
+
+
+// global let
+let html;
+
+
+// clears current global, creates html based on current array of objects
+function refreshList() {
+    html= "";
+
+
+    for (let i =0; i < addressBook.list.length; i++) {
+        html += `<ul class="contacts" id="contact${[i]}">
+                    <li>Name: ${addressBook.list[i].name}</li>
+                    <li>Email: ${addressBook.list[i].email}</li>
+                    <li>Phone: ${addressBook.list[i].phone}</li>
+                    <li>Relation: ${addressBook.list[i].relation}</li>
+                    <button id="x${[i]}">x</button>
+                </ul>`;
+
+    }
+
+    document.querySelector("#contacts").innerHTML = html;
+}
